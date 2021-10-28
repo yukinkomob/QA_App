1abae134641ee877dbb8f596e034264fa14bcf32
Nishimura <yukinkopen@gmail.com>
2021-10-28 15:49:29 +0900

    [add] ログイン画面のレイアウトを作成

---
+++ app/src/main/java/jp/techacademy/yuki/nishimura/qa_app/LoginActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.yuki.nishimura.qa_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
