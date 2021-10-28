6044ec334e2dda7a663cfe261d47925f1219b09e
Nishimura <yukinkopen@gmail.com>
2021-10-28 15:22:43 +0900

    [add] 定数定義ファイルを追加

---
+++ app/src/main/java/jp/techacademy/yuki/nishimura/qa_app/Const.kt
@@ -0,0 +1,9 @@
+package jp.techacademy.yuki.nishimura.qa_app
+
+const val UsersPATH = "users"
+
+const val ContentsPATH = "contents"
+
+const val AnswersPATH = "answers"
+
+const val NameKEY = "name"
